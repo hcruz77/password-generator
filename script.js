0450e4bff1ab3c06c3bb8be1d6ba95e97d693221
Gypsy Bird <110423762+hcruz77@users.noreply.github.com>
2022-08-17 17:12:53 -0400

    created prompts

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var passwordlength = Number(prompt("How long would you like for your password? (Between 8-128)"));
+  var lowerCase = prompt("Would you like lower case letters in your password?");
+  var upperCase = prompt("Would you like upper case letters in your password?");
+  var number = Number(prompt("Would you like numbers in your password?"));
+  var specialChar = prompt("Would you like special characters in your password?");
+
+}
+if(){
+  
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
